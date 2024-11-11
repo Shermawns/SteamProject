db3fbb205595f9fa0d7d3901cccfc10c358d540c
sherm <shermawns@gmail.com>
2024-11-11 16:22:10 -0300

    First commit

---
+++ src/main/java/com/Shermawn/GameTomato/GameTomatoApplication.java
@@ -0,0 +1,13 @@
+package com.Shermawn.GameTomato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameTomatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameTomatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Shermawn/GameTomato/GameTomatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Shermawn.GameTomato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameTomatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
