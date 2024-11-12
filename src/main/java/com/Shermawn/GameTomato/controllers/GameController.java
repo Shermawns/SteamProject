e45f2f83701f24fc56152db2a0fb6c12feb032d9
sherm <shermawns@gmail.com>
2024-11-12 10:55:27 -0300

    GameAllDto and GameController

--- src/main/java/com/Shermawn/GameTomato/controllers/GameController.java
+++ src/main/java/com/Shermawn/GameTomato/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.Shermawn.GameTomato.controllers;
 
+import com.Shermawn.GameTomato.dto.GameAllDTO;
 import com.Shermawn.GameTomato.dto.GameDTO;
 import com.Shermawn.GameTomato.models.Game;
 import com.Shermawn.GameTomato.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,12 @@ public class GameController {
     @GetMapping
     public List<GameDTO> findAll(){
         List<GameDTO> result = gameService.findAll();
+        return result;
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameAllDTO findById(@PathVariable Long id){
+        GameAllDTO result = gameService.findById(id);
         return result;
     }
 }
---
+++ src/main/java/com/Shermawn/GameTomato/dto/GameAllDTO.java
@@ -0,0 +1,74 @@
+package com.Shermawn.GameTomato.dto;
+
+import com.Shermawn.GameTomato.models.Game;
+import org.springframework.beans.BeanUtils;
+
+public class GameAllDTO {
+    private Long id;
+    private String title;
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+
+    public GameAllDTO(Game game) {
+        BeanUtils.copyProperties(game, this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}
